6fb1319a080f0f4aed6f8bb3f3929d858e8b709b
StepsonABI <164443508+StepsonABI@users.noreply.github.com>
2024-12-13 16:31:45 +0330

    worker.js

---
+++ worker.js
@@ -0,0 +1,1 @@
+[![Build and Deploy Worker](https://github.com/bia-pain-bache/BPB-Worker-Panel/actions/workflows/build.yml/badge.svg)](https://github.com/bia-pain-bache/BPB-Worker-Panel/actions/workflows/build.yml)
